cf79c40d25fc61804eaf0e836b2ee85f9ec35d22
Frank Tran <frank.tran@live.ca>
2021-09-02 23:14:48 -0400

    Adding Files

---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+
+const app = express();
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'Frank',
+      email: 'frank@gmail.com',
+      password: 'abc123',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Ricky',
+      email: 'ricky@gmail.com',
+      password: 'abc123',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.use(express.json());
+
+// HELPER FN
+
+// HOME
+app.get('/', (req, res) => {
+  res.json(database);
+});
+
+// LOGIN
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('Success');
+  } else {
+    res.status(400).json('Error Logging In');
+  }
+  res.json('Login is working');
+});
+
+// REGISTER
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  console.log(database);
+  res.json(
+    database.users[database.users.length - 1].name + ' has been registered'
+  );
+});
+
+// PROFILE
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json('User Not Found');
+  }
+});
+
+// IMAGE
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json('User Not Found');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('App is Running on Port 3000');
+});
