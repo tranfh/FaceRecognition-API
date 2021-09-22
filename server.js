e988248456a4c12196e29ccd459524018d0e92bb
Frank Tran <frank.tran@live.ca>
2021-09-22 17:20:08 -0400

    Adding Database Functionality

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'franktran',
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+// Test Connection
+// console.log(db.select('*').from('users'));
 
 const app = express();
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'Frank',
-      email: 'test',
-      password: 'test',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Ricky',
-      email: 'ricky@gmail.com',
-      password: 'abc123',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 app.use(express.json());
 app.use(cors());
 
@@ -36,62 +30,87 @@ app.get('/', (req, res) => {
 
 // LOGIN
 app.post('/login', (req, res) => {
-  console.log(req.body);
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('Error Logging In');
-  }
-  res.json('Login is working');
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          });
+      } else {
+        res.status(400).json('Error Logging In');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('Error Logging In');
+    })
+    .catch((err) => res.status(400).json('Invalid Credentials'));
 });
 
 // REGISTER
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
-  console.log(database);
-  res.json(database.users[database.users.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('Unable to Register'));
 });
 
 // PROFILE
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
   let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json('User Not Found');
-  }
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json('User Not Found');
+      }
+    })
+    .catch((err) => res.status(404).json('Error Retrieving Profile'));
 });
 
 // IMAGE
 app.put('/image', (req, res) => {
   const { id } = req.body;
   let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json('User Not Found');
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => res.json(entries[0]))
+    .catch((err) => {
+      res.status(404).json('Error Retrieving Entries');
+    });
 });
 
 app.listen(3000, () => {
